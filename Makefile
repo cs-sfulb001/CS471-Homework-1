37b3e1a384a8cf295a3c31fddfac65302d8d937a
Sean <sfulb001@odu.edu>
2024-09-13 17:20:57 -0400

    Coding section done

--- Makefile
+++ Makefile
@@ -5,12 +5,14 @@ CXXFlags = -pthread -unistd
 SRC = src/main.cpp
 target = my_program.sh
 
-all: Question2
+all: Question2 Question3 Question4
 
 Question2:
 	gcc src/Question2.c $(CXXFlags) -o Question2.out
 Question3:
 	gcc src/Question3.c $(CXXFlags) -o Question3.out
+Question4:
+	gcc src/Question4.c $(CXXFlags) -o Question4.out
 
 clean:
 	rm -f *.out
--- src/Question3.c
+++ src/Question3.c
@@ -2,33 +2,54 @@
 #include <pthread.h>
 #include <sys/types.h>
 #include <unistd.h>
-
 int main(){
     int elements[]={20, 18, 16, 14, 12, 10, 8, 6, 4, 2, -20, -18, -16, -14, -12, -10, -8, -6, -4, -2};
     int n=0;
-    pid_t p=getppid();
+    int p[2];
+    if(pipe(p)==-1){
+        printf("Pipe Failed");
+        return 1;
+    }
+    pid_t pid=getppid();
     for(int i=0;i<5;i++){
-        if(p!=0) {
-            p=fork();
+        if(pid!=0) {
+            pid=fork();
             n++;
         }
     }
-    if(p==0)n--;
+    if(pid==0)n--;
     printf("line\n");
     printf("%d\n", n);
     int sum=0;
-    if(p==0){
+    if(pid==0){
+        close(p[0]);
         for(int i=n*4; i<((n+1)*4);i++){
             sum+=elements[i];
         }
-        return sum;
+        printf("%d thread pushing %d into pipe\n", n, sum);
+        char pinput[3];
+        if(sum>0){
+            sprintf(pinput, "+%d", sum);
+        }
+        else{
+            sprintf(pinput, "%d", sum);
+        }
+        write(p[1], pinput, strlen(pinput));
+        close(p[1]);
+        return 0;
     }
     else{
+        close(p[1]);
+        while(wait(NULL)!=-1){}
+        printf("Reading from pipe\n");
         for(int i=0;i<5;i++){
-            int status;
-            wait(&status);
-            sum+=WEXITSTATUS(status);
+            char value[3];
+            read(p[0], value, 3);
+            printf("%s\n", value);
+            sum+=atoi(value);
         }
+        printf("Done reading\n");
+        close(p[0]);
     }
-    printf("The sum is %d", sum);
+    printf("The sum is %d\n", sum);
 }
